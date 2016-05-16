6fa7a927d3998a90c8341dda42ee4eea3955ffeb
Andrea Zito <andrea.zito@u-hopper.com>
2016-05-16 21:01:00 +0200

    Fixes #1 by providing fallback timestamp format with milliseconds

--- datatxt-core/src/main/scala/io/github/nivox/dandelion/datatxt/CommonCodecs.scala
+++ datatxt-core/src/main/scala/io/github/nivox/dandelion/datatxt/CommonCodecs.scala
@@ -16,10 +16,18 @@ object CommonCodecs {
     df
   }
 
+  val timestampWithMillisDateFormat = {
+    val df = new SimpleDateFormat("YYYY-MM-dd'T'HH:mm:ss.SSS")
+    df.setTimeZone(TimeZone.getTimeZone("GMT+0"))
+    df
+  }
+
   def getTimestamp(c: HCursor): DecodeResult[Date] = {
     for {
       timestampStr <- c.get[String]("timestamp") ||| DecodeResult.fail("Missing or invalid timestamp", c.history)
-      timestamp <- \/.fromTryCatchNonFatal(timestampDateFormat.parse(timestampStr)).fold(
+      maybeTimestamp = \/.fromTryCatchNonFatal(timestampDateFormat.parse(timestampStr)) |||
+        \/.fromTryCatchNonFatal(timestampWithMillisDateFormat.parse(timestampStr))
+      timestamp <- maybeTimestamp.fold(
         _ => DecodeResult.fail(s"Invalid timestamp format: ${timestampStr}", new CursorHistory(List())),
         date => DecodeResult.ok(date)
       )
